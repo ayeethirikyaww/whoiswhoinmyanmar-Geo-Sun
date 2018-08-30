6a3ff3620a9dec05fe3a7004435d14cde079e967
ayeethirikyaww <atkyaw.comp@gmail.com>
2018-08-30 21:10:07 +0630

    realproj

--- main.py
+++ main.py
@@ -1,83 +1,93 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
+import os 
+from flask_sqlalchemy import SQLAlchemy 
 
-app=Flask(__name__)
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "Personal.db"))
 
-@app.route('/')
-@app.route("/index")
-def hello():
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = "Who is Who in Myanmar"
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
     return render_template("index.html")
 
-@app.route("/about")
+search = ""
+@app.route('/', methods=['POST', 'GET'])
+def search():
+    people = Person.query.all()
+
+    search = request.form['search']
+    names = Person.query.order_by(Person.name).all()
+    results = Person.query.filter(Person.name.contains(search)).all()
+    return render_template("search.html", results=results)
+
+@app.route('/aboutparli.html', methods=['POST', 'GET'])
+def politician():
+    politician = Person.query.filter(Person.profession=="politician").all()
+    return render_template("aboutparli.html", politician=politician)
+
+@app.route('/aboutwriter.html', methods=['POST', 'GET'])
+def writer():
+    writer = Person.query.filter(Person.profession=="writer").all()
+    return render_template("aboutwriter.html", writer=writer)
+
+@app.route('/aboutdirector.html', methods=['POST', 'GET'])
+def director():
+    director = Person.query.filter(Person.profession=="director").all()
+    return render_template("aboutdirector.html", director=director)
+    
+@app.route('/aboutactor.html', methods=['POST', 'GET'])
+def actor():
+    actor = Person.query.filter(Person.profession=="actor").all()
+    return render_template("aboutactor.html", actor=actor)
+
+@app.route('/aboutsinger.html', methods=['POST', 'GET'])
+def singer():
+    singer = Person.query.filter(Person.profession=="singer").all()
+    return render_template("aboutsinger.html", singer=singer)
+
+@app.route("/about.html", methods=['POST', 'GET'])
 def about():
     return render_template("about.html")
 
-@app.route("/aboutactor")
-def aboutactor():
-    return render_template("aboutactor.html")
-
-@app.route("/aboutdirector")
-def aboutdirector():
-    return render_template("aboutdirector.html")
-
-@app.route("/aboutparli")
-def aboutparli():
-    return render_template("aboutparli.html")
-
-@app.route("/aboutsinger")
-def aboutsinger():
-    return render_template("aboutsinger.html")
-
-@app.route("/aboutwriter")
-def aboutwriter():
-    return render_template("aboutwriter.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/help")
+@app.route("/help.html", methods=['POST', 'GET'])
 def help():
     return render_template("help.html")
 
-@app.route("/method")
+@app.route("/method.html", methods=['POST', 'GET'])
 def method():
     return render_template("method.html")
 
-@app.route("/moreactor")
-def moreactor():
-    return render_template("moreactor.html")
-
-@app.route("/moredirector")
-def moredirector():
-    return render_template("moredirector.html")
-
-@app.route("/moreparli")
-def moreparli():
-    return render_template("moreparli.html")
-
-@app.route("/moresinger")
-def moresinger():
-    return render_template("moresinger.html")
-
-@app.route("/morewriter")
-def morewriter():
-    return render_template("morewriter.html")
-
-@app.route("/partner")
-def partner():
-    return render_template("partner.html")
-
-@app.route("/qa")
-def qa():
-    return render_template("qa.html")
-
-@app.route("/send")
-def send():
-    return render_template("send.html")
-
-@app.route("/use")
+@app.route("/use.html", methods=['POST', 'GET'])
 def use():
     return render_template("use.html")
 
+@app.route("/contact.html", methods=['POST', 'GET'])
+def contact():
+    return render_template("contact.html")
+
+@app.route("/qa.html", methods=['POST', 'GET'])
+def qa():
+    return render_template("qa.html")
+
+@app.route("/partner.html", methods=['POST', 'GET'])
+def partner():
+    return render_template("partner.html")
+
+class Person(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(30),nullable=False)
+    profession = db.Column(db.String(30),nullable=False)
+    image = db.Column(db.Text,nullable=False)
+    contact = db.Column(db.Text,nullable=False)
+
+    def __init__(self,*args,**kwargs):
+        super(Person,self).__init__(*args,**kwargs)
+    
+    def __repr__(self):
+        return "<Person: {}>".format(self.name)
 if __name__=="__main__":
     app.run(debug=True)
